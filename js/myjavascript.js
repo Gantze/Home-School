51b90443d21ec2db575b6c62289d198ffd3aa809
Raul Ganță <raulganta94@yahoo.com>
2022-10-18 15:28:44 +0300

    Updating style.css, index.html and myjavascript.js files
    Code refactoring

--- js/myjavascript.js
+++ js/myjavascript.js
@@ -18,29 +18,33 @@ function menuBarClick() {
 
 
 // Timer section:
-let timerDays = document.getElementById('timer-days');
-let timerHours = document.getElementById('timer-hours');
-let timerMinutes = document.getElementById('timer-minutes');
-let timerSeconds = document.getElementById('timer-seconds');
-let countDownDate = new Date('Dec 31, 2022 23:59:59').getTime();
+function countdown(){
+    let countDate = new Date('Dec 31, 2022, 23:59:59').getTime();
+    let today = new Date().getTime();
+    let distance = countDate - today;
 
-let timePass = setInterval(function() {
-        let today = new Date().getTime();
-        let distance = countDownDate - today;
-    if (distance < 0) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    if(distance < 0) {
         clearInterval(timePass);
-        timer.innerHTML = `Time is out`;
-    } else {     
-        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        timerDays.innerHTML = days;
-        timerHours.innerHTML = hours;
-        timerMinutes.innerHTML = minutes;
-        timerSeconds.innerHTML = seconds;
+        document.getElementById('timer-container').innerHTML = `Time is out`;
+    } else {
+        let textDay = Math.floor(distance / day);
+        let textHour = Math.floor((distance % day) / hour);
+        let textMinute = Math.floor((distance % hour) / minute);
+        let textSecond = Math.floor((distance % minute) / second);
+
+        document.getElementById('timer-days').innerHTML= textDay;
+        document.getElementById('timer-hours').innerHTML= textHour;
+        document.getElementById('timer-minutes').innerHTML= textMinute;
+        document.getElementById('timer-seconds').innerHTML= textSecond;
     }
-}, 1000);
+}
+
+let timePass = setInterval(countdown, 1000);
 
 
 
@@ -78,103 +82,15 @@ function monthlySubscription() {
 
 
 // FAQ section:
-let arrowUpOne = document.getElementById('arrow-up-1');
-let arrowUpTwo = document.getElementById('arrow-up-2');
-let arrowUpThree = document.getElementById('arrow-up-3');
-let arrowUpFour = document.getElementById('arrow-up-4');
-let arrowUpFive = document.getElementById('arrow-up-5');
 
-let arrowDownOne = document.getElementById('arrow-down-1');
-let arrowDownTwo = document.getElementById('arrow-down-2');
-let arrowDownThree = document.getElementById('arrow-down-3');
-let arrowDownFour = document.getElementById('arrow-down-4');
-let arrowDownFive = document.getElementById('arrow-down-5');
+let accordion = document.getElementsByClassName('accordion');
 
-let answerOne = document.getElementById('answer1');
-let answerTwo = document.getElementById('answer2');
-let answerThree = document.getElementById('answer3');
-let answerFour = document.getElementById('answer4');
-let answerFive = document.getElementById('answer5');
-
-//for Question-Answer 1:
-arrowDownOne.addEventListener('click', () => showAnswerOne());
-arrowUpOne.addEventListener('click', () => hideAnswerOne());
-
-function showAnswerOne() {
-    answerOne.style.display = 'block';
-    arrowUpOne.style.display = 'block';
-    arrowDownOne.style.display = 'none';
+for(let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+    });
 }
 
-function hideAnswerOne() {
-    answerOne.style.display = 'none';
-    arrowUpOne.style.display = 'none';
-    arrowDownOne.style.display = 'block';
-}
-
-//for Question-Answer 2:
-arrowDownTwo.addEventListener('click', () => showAnswerTwo());
-arrowUpTwo.addEventListener('click', () => hideAnswerTwo());
-
-function showAnswerTwo() {
-    answerTwo.style.display = 'block';
-    arrowUpTwo.style.display = 'block';
-    arrowDownTwo.style.display = 'none';
-}
-
-function hideAnswerTwo() {
-    answerTwo.style.display = 'none';
-    arrowUpTwo.style.display = 'none';
-    arrowDownTwo.style.display = 'block';
-}
-
-//for Question-Answer 3:
-arrowDownThree.addEventListener('click', () => showAnswerThree());
-arrowUpThree.addEventListener('click', () => hideAnswerThree());
-
-function showAnswerThree() {
-    answerThree.style.display = 'block';
-    arrowUpThree.style.display = 'block';
-    arrowDownThree.style.display = 'none';
-}
-
-function hideAnswerThree() {
-    answerThree.style.display = 'none';
-    arrowUpThree.style.display = 'none';
-    arrowDownThree.style.display = 'block';
-}
-
-//for Question-Answer 4:
-arrowDownFour.addEventListener('click', () => showAnswerFour());
-arrowUpFour.addEventListener('click', () => hideAnswerFour());
-
-function showAnswerFour() {
-    answerFour.style.display = 'block';
-    arrowUpFour.style.display = 'block';
-    arrowDownFour.style.display = 'none';
-}
-
-function hideAnswerFour() {
-    answerFour.style.display = 'none';
-    arrowUpFour.style.display = 'none';
-    arrowDownFour.style.display = 'block';
-}
-
-//for Question-Answer 5:
-arrowDownFive.addEventListener('click', () => showAnswerFive());
-arrowUpFive.addEventListener('click', () => hideAnswerFive());
-
-function showAnswerFive() {
-    answerFive.style.display = 'block';
-    arrowUpFive.style.display = 'block';
-    arrowDownFive.style.display = 'none';
-}
-
-function hideAnswerFive() {
-    answerFive.style.display = 'none';
-    arrowUpFive.style.display = 'none';
-    arrowDownFive.style.display = 'block';
-}
 
 
     
